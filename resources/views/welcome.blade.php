6849e822e8e384b4680c30a8e2b2b67b987bab33
qq1052663897 <1052663897@qq.com>
2019-09-11 22:30:49 +0800

    welcome changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Tflybaby
+                    Hello Tflybaby
                 </div>
 
                 <div class="links">
